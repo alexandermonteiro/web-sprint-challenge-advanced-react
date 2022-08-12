f4ee8084dd4a0a948ad9809b29087f68b94088a8
Alexander Monteiro <alexandermonteiro@Priscilas-MBP.attlocal.net>
2022-08-12 15:06:45 -0700

    appclass

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,51 +1,54 @@
 import React from "react";
+import axios from "axios";
 
 // Suggested initial states
 const initialMessage = "";
 const initialEmail = "";
 const initialSteps = 0;
 const initialIndex = 4; // the index the "B" is at
+const initialXY = { x: 2, y: 2 };
+const initialDirection = "";
 
-const initialState = {
-  message: initialMessage,
-  email: initialEmail,
-  index: initialIndex,
-  steps: initialSteps,
-};
+const URL = `http://localhost:9000/api/result`;
 
 export default class AppClass extends React.Component {
   state = {
-    initialIndex: 4,
-    initialMessage: "",
-    initialEmail: "",
-    initialSteps: 0,
-    xCoordinate: 2,
-    yCoordinate: 2,
+    index: initialIndex,
+    message: initialMessage,
+    email: initialEmail,
+    steps: initialSteps,
+    xy: initialXY,
+    direction: initialDirection,
   };
 
-  // THE FOLLOWING HELPERS ARE JUST RECOMMENDATIONS.
-  // You can delete them and build your own logic from scratch.
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.index !== this.state.index) {
+      this.getXY(this.state.index);
+    }
+  }
 
-  getX = (xCoordinate) => {
-    if (xCoordinate === 0 || (xCoordinate === 1) | (xCoordinate === 2))
-      return xCoordinate + 1;
-    if (xCoordinate === 3 || xCoordinate === 4 || xCoordinate === 5)
-      return xCoordinate - 2;
-    if (xCoordinate === 6 || xCoordinate === 7 || xCoordinate === 8)
-      return xCoordinate - 5;
-
+  getXY = (gridIndex) => {
     // It it not necessary to have a state to track the coordinates.
     // It's enough to know what index the "B" is at, to be able to calculate them.
-  };
-
-  getY = (yCoordinate) => {
-    if (yCoordinate === 0) return yCoordinate + 1;
-    if (yCoordinate === 1) return yCoordinate;
-    if (yCoordinate === 2 || yCoordinate === 3) return yCoordinate - 1;
-    if (yCoordinate === 4) return yCoordinate - 2;
-    if (yCoordinate === 5 || yCoordinate === 6) return yCoordinate - 3;
-    if (yCoordinate === 7) return yCoordinate - 4;
-    if (yCoordinate === 8) return yCoordinate - 5;
+    if (gridIndex === 0) {
+      this.setState({ ...this.state, xy: { x: 1, y: 1 } });
+    } else if (gridIndex === 1) {
+      this.setState({ ...this.state, xy: { x: 2, y: 1 } });
+    } else if (gridIndex === 2) {
+      this.setState({ ...this.state, xy: { x: 3, y: 1 } });
+    } else if (gridIndex === 3) {
+      this.setState({ ...this.state, xy: { x: 1, y: 2 } });
+    } else if (gridIndex === 4) {
+      this.setState({ ...this.state, xy: { x: 2, y: 2 } });
+    } else if (gridIndex === 5) {
+      this.setState({ ...this.state, xy: { x: 3, y: 2 } });
+    } else if (gridIndex === 6) {
+      this.setState({ ...this.state, xy: { x: 1, y: 3 } });
+    } else if (gridIndex === 7) {
+      this.setState({ ...this.state, xy: { x: 2, y: 3 } });
+    } else if (gridIndex === 8) {
+      this.setState({ ...this.state, xy: { x: 3, y: 3 } });
+    }
   };
 
   getXYMessage = () => {
@@ -56,160 +59,92 @@ export default class AppClass extends React.Component {
 
   reset = () => {
     this.setState({
-      initialIndex: 4,
-      initialMessage: "",
-      initialEmail: "",
-      initialSteps: 0,
-      xCoordinate: 2,
-      yCoordinate: 2,
+      ...this.state,
+      index: initialIndex,
+      message: initialMessage,
+      email: initialEmail,
+      steps: initialSteps,
     });
     // Use this helper to reset all states to their initial values.
   };
 
   getNextIndex = (direction) => {
-    if (direction === "left" && this.state.initialIndex === 0)
-      return this.state.initialIndex;
-    if (direction === "left" && this.state.initialIndex === 3)
-      return this.state.initialIndex;
-    if (direction === "left" && this.state.initialIndex === 6)
-      return this.state.initialIndex;
-
-    if (direction === "right" && this.state.initialIndex === 2)
-      return this.state.initialIndex;
-    if (direction === "right" && this.state.initialIndex === 5)
-      return this.state.initialIndex;
-    if (direction === "right" && this.state.initialIndex === 8)
-      return this.state.initialIndex;
-
-    if (direction === "up" && this.state.initialIndex === 0)
-      return this.state.initialIndex;
-    if (direction === "up" && this.state.initialIndex === 1)
-      return this.state.initialIndex;
-    if (direction === "up" && this.state.initialIndex === 2)
-      return this.state.initialIndex;
-
-    if (direction === "down" && this.state.initialIndex === 6)
-      return this.state.initialIndex;
-    if (direction === "down" && this.state.initialIndex === 7)
-      return this.state.initialIndex;
-    if (direction === "down" && this.state.initialIndex === 8)
-      return this.state.initialIndex;
-
-    if (direction === "left") return this.state.initialIndex - 1;
-    if (direction === "right") return this.state.initialIndex + 1;
-    if (direction === "up") return this.state.initialIndex - 3;
-    if (direction === "down") return this.state.initialIndex + 3;
-
-    helperSteps = (direction) => {
-      if (direction === "left" && this.state.initialIndex === 1)
-        return this.state.initialSteps + 1;
-      if (direction === "left" && this.state.initialIndex === 4)
-        return this.state.initialSteps + 1;
-      if (direction === "left" && this.state.initialIndex === 7)
-        return this.state.initialSteps + 1;
-      if (direction === "left" && this.state.initialIndex === 2)
-        return this.state.initialSteps + 1;
-      if (direction === "left" && this.state.initialIndex === 5)
-        return this.state.initialSteps + 1;
-      if (direction === "left" && this.state.initialIndex === 8)
-        return this.state.initialSteps + 1;
-
-      if (direction === "right" && this.state.initialIndex === 0)
-        return this.state.initialSteps + 1;
-      if (direction === "right" && this.state.initialIndex === 3)
-        return this.state.initialSteps + 1;
-      if (direction === "right" && this.state.initialIndex === 6)
-        return this.state.initialSteps + 1;
-      if (direction === "right" && this.state.initialIndex === 1)
-        return this.state.initialSteps + 1;
-      if (direction === "right" && this.state.initialIndex === 4)
-        return this.state.initialSteps + 1;
-      if (direction === "right" && this.state.initialIndex === 7)
-        return this.state.initialSteps + 1;
-
-      if (direction === "up" && this.state.initialIndex === 3)
-        return this.state.initialSteps + 1;
-      if (direction === "up" && this.state.initialIndex === 4)
-        return this.state.initialSteps + 1;
-      if (direction === "up" && this.state.initialIndex === 5)
-        return this.state.initialSteps + 1;
-      if (direction === "up" && this.state.initialIndex === 6)
-        return this.state.initialSteps + 1;
-      if (direction === "up" && this.state.initialIndex === 7)
-        return this.state.initialSteps + 1;
-      if (direction === "up" && this.state.initialIndex === 8)
-        return this.state.initialSteps + 1;
-
-      if (direction === "down" && this.state.initialIndex === 0)
-        return this.state.initialSteps + 1;
-      if (direction === "down" && this.state.initialIndex === 1)
-        return this.state.initialSteps + 1;
-      if (direction === "down" && this.state.initialIndex === 2)
-        return this.state.initialSteps + 1;
-      if (direction === "down" && this.state.initialIndex === 3)
-        return this.state.initialSteps + 1;
-      if (direction === "down" && this.state.initialIndex === 4)
-        return this.state.initialSteps + 1;
-      if (direction === "down" && this.state.initialIndex === 5)
-        return this.state.initialSteps + 1;
-      else return this.state.initialSteps;
-    };
-
-    errorHelper = (direction) => {
-      if (direction === "left" && this.state.initialIndex === 0)
-        return "You can't go left";
-      if (direction === "left" && this.state.initialIndex === 3)
-        return "You can't go left";
-      if (direction === "left" && this.state.initialIndex === 6)
-        return "You can't go left";
-
-      if (direction === "up" && this.state.initialIndex === 0)
-        return "You can't go up";
-      if (direction === "up" && this.state.initialIndex === 1)
-        return "You can't go up";
-      if (direction === "up" && this.state.initialIndex === 2)
-        return "You can't go up";
-
-      if (direction === "down" && this.state.initialIndex === 6)
-        return "You can't go down";
-      if (direction === "down" && this.state.initialIndex === 7)
-        return "You can't go down";
-      if (direction === "down" && this.state.initialIndex === 8)
-        return "You can't go down";
-
-      if (direction === "right" && this.state.initialIndex === 2)
-        return "You can't go right";
-      if (direction === "right" && this.state.initialIndex === 5)
-        return "You can't go right";
-      if (direction === "right" && this.state.initialIndex === 8)
-        return "You can't go right";
-    };
-    // This helper takes a direction ("left", "up", etc) and calculates what the next index
-    // of the "B" would be. If the move is impossible because we are at the edge of the grid,
-    // this helper should return the current index unchanged.
+    if (direction === "left" && this.state.xy.x !== 1) {
+      this.setState({
+        index: this.state.index - 1,
+        message: "",
+        steps: this.state.steps + 1,
+      });
+    } else if (direction === "left" && this.state.xy.x === 1) {
+      this.setState({
+        message: "You can't go left",
+      });
+    } else if (direction === "right" && this.state.xy.x !== 3) {
+      this.setState({
+        index: this.state.index + 1,
+        message: "",
+        steps: this.state.steps + 1,
+      });
+    } else if (direction === "right" && this.state.xy.x === 3) {
+      this.setState({
+        message: "You can't go right",
+      });
+    } else if (direction === "up" && this.state.xy.y !== 1) {
+      this.setState({
+        index: this.state.index - 3,
+        message: "",
+        steps: this.state.steps + 1,
+      });
+    } else if (direction === "up" && this.state.xy.y === 1) {
+      this.setState({
+        message: "You can't go up",
+      });
+    } else if (direction === "down" && this.state.xy.y !== 3) {
+      this.setState({
+        index: this.state.index + 3,
+        message: "",
+        steps: this.state.steps + 1,
+      });
+    } else if (direction === "down" && this.state.xy.y === 3) {
+      this.setState({
+        message: "You can't go down",
+      });
+    }
   };
 
   move = (evt) => {
-    this.setState({
-      ...this.state,
-      initialIndex: this.getNextIndex(evt.target.id),
-      initialSteps: this.helperSteps(evt.target.id),
-      xCoordinate: this.getX(this.state.initialIndex),
-      yCoordinate: this.getY(this.state.initialIndex),
-      initialMessage: this.errorHelper(evt.target.id),
-    });
-    // This event handler can use the helper above to obtain a new index for the "B",
-    // and change any states accordingly.
+    this.getNextIndex(evt.target.id);
+    this.setState({ direction: evt.target.id });
   };
+  // This event handler can use the helper above to obtain a new index for the "B",
+  // and change any states accordingly.
 
   onChange = (evt) => {
     // You will need this to update the value of the input.
-    const { value } = evt.target;
-    this.setState({ ...this.state, initialEmail: value });
+    this.setState({ ...this.state, email: evt.target.value });
   };
 
   onSubmit = (evt) => {
     // Use a POST request to send a payload to the server.
+    evt.preventDefault();
+    const newSubmission = {
+      x: this.state.xy.x,
+      y: this.state.xy.y,
+      steps: this.state.steps,
+      email: this.state.email,
+    };
+
+    axios
+      .post(URL, newSubmission)
+      .then((res) => {
+        this.setState({ ...this.state, message: res.data.message });
+      })
+      .catch((err) => {
+        this.setState({ ...this.state, message: err.response.data.message });
+      })
+      .finally(() => {
+        this.setState({ email: "" });
+      });
   };
 
   render() {
@@ -218,29 +153,27 @@ export default class AppClass extends React.Component {
       <div id="wrapper" className={className}>
         <div className="info">
           <h3 id="coordinates">
-            Coordinates ({this.getX(this.state.initialIndex)},
-            {this.getY(this.state.initialIndex)})
+            {`Coordinates (${this.state.xy.x},
+            ${this.state.xy.y})`}
           </h3>
           {/* <h3 id="steps">You moved {this.state.initialSteps} times</h3> */}
           <h3 id="steps">
-            You moved {this.state.initialSteps}{" "}
-            {this.state.initialSteps === 1 ? "time" : "times"}
+            You moved {this.state.steps}{" "}
+            {this.state.steps === 1 ? "time" : "times"}
           </h3>
         </div>
         <div id="grid">
           {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
             <div
               key={idx}
-              className={`square${
-                idx === this.state.initialIndex ? " active" : ""
-              }`}
+              className={`square${idx === this.state.index ? " active" : ""}`}
             >
-              {idx === this.state.initialIndex ? "B" : null}
+              {idx === this.state.index ? "B" : null}
             </div>
           ))}
         </div>
         <div className="info">
-          <h3 id="message">{this.state.initialMessage}</h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
           <button onClick={this.move} id="left">
@@ -265,7 +198,7 @@ export default class AppClass extends React.Component {
             type="email"
             placeholder="type email"
             onChange={this.onChange}
-            value={this.state.initialEmail}
+            value={this.state.email}
           ></input>
           <input id="submit" type="submit"></input>
         </form>
